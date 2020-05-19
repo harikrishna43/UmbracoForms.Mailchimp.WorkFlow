a316916cd41c9c092405365eae3be7f9a150beae
Thomas Adams <thomas.adams@gibedigital.com>
2020-05-19 15:22:11 +0100

    Set AssemblyCulture in AssemblyInfo to default

--- UFMailchimpWorkFlowType/Properties/AssemblyInfo.cs
+++ UFMailchimpWorkFlowType/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("UFMailchimpWorkFlowType")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("in")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
