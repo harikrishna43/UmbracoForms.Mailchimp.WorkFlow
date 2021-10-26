61fe8b0e6b3ad16ad11f168f31733066cb1854e7
Aaron Sadler <aaron@aaronsadler.uk>
2021-10-26 14:35:21 +0100

    Initial Build

---
+++ Our.Umbraco.Forms.Mailchimp.Workflow/Extensions/UmbracoBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Our.Umbraco.Forms.Mailchimp.Workflow.Workflows;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Forms.Core.Providers;
+
+namespace Our.Umbraco.Forms.Mailchimp.Workflow.Extensions
+{
+    public static class UmbracoBuilderExtensions
+    {
+        public static IUmbracoBuilder AddMailChimpWorkFlowType(this IUmbracoBuilder builder)
+        {
+            builder.WithCollectionBuilder<WorkflowCollectionBuilder>().Add<MailChimpWorkFlowType>();
+            return builder;
+        }
+    }
+}
